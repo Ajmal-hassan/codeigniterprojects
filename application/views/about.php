1b83ed7bf08a031daad86a13670644597675897a
ajmal\Ajmal hassan <ajmalhassan651@gmail.com>
2018-10-16 18:29:56 +0500

    Profile done

---
+++ application/views/about.php
@@ -0,0 +1,96 @@
+<section id="about">  
+
+   	<div class="row section-intro">
+   		<div class="col-twelve">
+
+   			<h5>About</h5>
+   			<h1>Let me introduce myself.</h1>
+
+   			<div class="intro-info">
+                            <img src="<?php echo(base_url());?>images/profile-image.png" alt="Profile Picture">
+                            <p class="lead">
+                                Professional Web-developer having a 2 years experience in web development. Also Expert in HTML5, CSS3, Bootstrap3, JavaScript, Php & MySQL, Wordpress and Google Blogger.
+                                I am providing exceptionally good services to my clients, especially to Small & intermediate Businesses with a small budget. I like to face challenges in the field of coding and web development.
+                            </p>
+   			</div>   			
+
+   		</div>   		
+   	</div> <!-- /section-intro -->
+
+   	<div class="row about-content">
+
+   		<div class="col-six tab-full">
+
+   			<h3>Profile</h3>
+                        <p>
+                            I am providing exceptionally good services to my clients, especially to Small & intermediate Businesses with a small budget.
+                        </p>
+
+   			<ul class="info-list">
+   				<li>
+   					<strong>Fullname:</strong>
+   					<span>Muhammad Ajmal</span>
+   				</li>
+   				<li>
+   					<strong>Birth Date:</strong>
+   					<span>April 01, 1995</span>
+   				</li>
+   				<li>
+   					<strong>Job:</strong>
+   					<span>Freelancer, Back-end Developer</span>
+   				</li>
+   				<li>
+   					<strong>Website:</strong>
+   					<span>www.betabutter.com</span>
+   				</li>
+   				<li>
+   					<strong>Email:</strong>
+   					<span>ajmalhassan651@gmail.com</span>
+   				</li>
+
+   			</ul> <!-- /info-list -->
+
+   		</div>
+
+   		<div class="col-six tab-full">
+
+   			<h3>Skills</h3>
+   			<p>Lorem ipsum Qui veniam ut consequat ex ullamco nulla in non ut esse in magna sint minim officia consectetur nisi commodo ea magna pariatur nisi cillum.</p>
+
+				<ul class="skill-bars">
+				   <li>
+				   	<div class="progress percent90"><span>90%</span></div>
+				   	<strong>HTML5</strong>
+				   </li>
+				   <li>
+				   	<div class="progress percent85"><span>85%</span></div>
+				   	<strong>CSS3</strong>
+				   </li>
+				   <li>
+				   	<div class="progress percent70"><span>70%</span></div>
+				   	<strong>JQuery</strong>
+				   </li>
+				   <li>
+				   	<div class="progress percent95"><span>95%</span></div>
+				   	<strong>PHP</strong>
+				   </li>
+				   <li>
+
+				   	<div class="progress percent95"><span>90%</span></div>
+				   	<strong>Wordpress</strong>
+				   </li>
+      
+				</ul> <!-- /skill-bars -->		
+
+   		</div>
+
+   	</div>
+
+   	<div class="row button-section">
+   		<div class="col-twelve">
+   			<a href="#contact" title="Hire Me" class="button stroke smoothscroll">Hire Me</a>
+   			<a href="#" title="Download CV" class="button button-primary">Download CV</a>
+   		</div>   		
+   	</div>
+
+   </section> <!-- /process-->  
---
+++ application/views/top.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- basic page needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Ajmal</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+
+   <!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+ 	<!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="<?php echo(base_url('css/base.css'));?>">  
+   <link rel="stylesheet" href="<?php echo(base_url('css/main.css'));?>">
+   <link rel="stylesheet" href="<?php echo(base_url('css/vendor.css'));?>">     
+
+   <!-- script
+   ================================================== -->   
+	<script src="<?php echo(base_url('js/modernizr.js'));?>"></script>
+	<script src="<?php echo(base_url('js/pace.min.js'));?>"></script>
+
+   <!-- favicons
+	================================================== -->
+	<link rel="icon" type="image/png" href="<?php echo(base_url('favicon.png'));?>">
+
+</head>
